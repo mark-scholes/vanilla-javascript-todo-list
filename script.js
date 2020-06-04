48d9b7078e07fd9caa5e43f3f0b1ce5650c9b95c
mark-scholes <innominatus15@outlook.com>
2020-06-04 02:09:59 +0100

    Added the ability to select (all, completed and incomplete) taks from a dropdown. Also taks are now saved in local storage when they are created and removed from local storage when they are deleted with the UI

--- script.js
+++ script.js
@@ -3,11 +3,13 @@
 const toDoInput = document.querySelector(".todo-input-box");
 const addTodos = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".fiter-todos");
 
 //Event listeners
-
+document.addEventListener("DOMContentLoaded", getTodos);
 addTodos.addEventListener("click", addTodo);
 todoList.addEventListener("click", alterTodo);
+filterOption.addEventListener("click", filterTodo);
 
 //Funcitons
 function addTodo(e) {
@@ -21,6 +23,8 @@ function addTodo(e) {
   todoItem.classList.add("todo-text");
   todoItem.innerText = toDoInput.value;
   todoDiv.appendChild(todoItem);
+  // Add todo to local storage
+  saveToLocalStorage(toDoInput.value);
   //create buttons
   const deleteButton = document.createElement("button");
   deleteButton.classList.add("delete-todo");
@@ -41,6 +45,7 @@ function alterTodo(e) {
   // delete todo
   if (item.classList[0] === "delete-todo") {
     const parent = item.parentElement;
+    removeLocalTodos(parent);
     //Animate delete transition
     parent.classList.add("deleting");
     parent.addEventListener("transitionend", () => {
@@ -52,4 +57,72 @@ function alterTodo(e) {
     const parent = item.parentElement;
     parent.classList.toggle("completed");
   }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (i) {
+    switch (e.target.value) {
+      case "all":
+        i.style.display = "flex";
+        break;
+      case "completed":
+        i.style.display = i.classList.contains("completed") ? "flex" : "none";
+        break;
+      case "incomplete":
+        i.style.display = !i.classList.contains("completed") ? "flex" : "none";
+    }
+  });
+}
+
+function checkLocalStorage() {
+  //check for existing todos
+  let currentTodos;
+  if (localStorage.getItem("currentTodos") === null) {
+    currentTodos = [];
+  } else {
+    currentTodos = JSON.parse(localStorage.getItem("currentTodos"));
+  }
+  return currentTodos;
+}
+
+function saveToLocalStorage(todo) {
+  let currentTodos = checkLocalStorage();
+  // Save to local storage
+  currentTodos.push(todo);
+  localStorage.setItem("currentTodos", JSON.stringify(currentTodos));
+}
+
+function getTodos() {
+  let currentTodos = checkLocalStorage();
+  currentTodos.forEach((todo) => {
+    //Todo Div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    //Todo Li
+    const todoItem = document.createElement("li");
+    todoItem.classList.add("todo-text");
+    todoItem.innerText = todo;
+    todoDiv.appendChild(todoItem);
+    //create buttons
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-todo");
+    deleteButton.innerHTML = "<i class='fas fa-trash'></i>";
+    const completeButton = document.createElement("button");
+    completeButton.classList.add("complete-todo");
+    completeButton.innerHTML = "<i class='fas fa-check'></i>";
+    todoDiv.appendChild(completeButton);
+    todoDiv.appendChild(deleteButton);
+
+    //append created todo to list
+    todoList.appendChild(todoDiv);
+  });
+}
+
+function removeLocalTodos(todo) {
+  let currentTodos = checkLocalStorage();
+  let elementToRemove = todo.children[0].innerText;
+  currentTodos = currentTodos.filter((item) => item !== elementToRemove);
+
+  localStorage.setItem("currentTodos", JSON.stringify(currentTodos));
 }
